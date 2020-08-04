7b8ac62b1ef8e6c51ec9a5f8e263306dd511984e
mrduhart <9971552+mrduhart@users.noreply.github.com>
2020-08-04 06:53:01 -0500

    modify detail index template

--- tutorial/polls/views.py
+++ tutorial/polls/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import get_object_or_404, render
 
 from .models import Question
 
+
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {
@@ -11,16 +12,16 @@ def index(request):
     }
     return render(request, 'polls/index.html', context)
 
+
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
+
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
     return HttpResponse(response % question_id)
+
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
